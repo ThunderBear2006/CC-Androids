c7788091c7f30211c613707e9023b9d3ff1b42dc
thunderbear <bearthunder06@gmail.com>
2025-05-26 23:32:55 -0400

    Yeah its been a few months this might be broken

--- src/main/java/com/thunderbear06/recipe/construction/ConstructionRecipeManager.java
+++
@@ -1,9 +0,0 @@
-package com.thunderbear06.recipe.construction;
-
-public class ConstructionRecipeManager {
-    public static final ConstructionRecipe RECIPE = new ConstructionRecipe();
-
-//    public static ConstructionRecipe registerRecipeFromFile() {
-//
-//    }
-}
